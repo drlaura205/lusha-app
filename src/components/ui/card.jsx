fdb752bbc1f4e6742bc47383f7f2c6bf155aca99
drlaura205 <clinicalmedicine037@gmail.com>
2025-07-08 04:03:39 +0300

    Delete src directory

--- src/components/ui/card.jsx
+++
@@ -1,6 +0,0 @@
-export const Card = ({ children, className }) => (
-  <div className={`bg-white border border-gray-200 rounded-lg ${className}`}>{children}</div>
-);
-export const CardContent = ({ children, className }) => (
-  <div className={`p-4 ${className}`}>{children}</div>
-);
