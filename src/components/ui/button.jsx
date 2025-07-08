2f01338de30b5cde50614559dd78579e61e17db2
drlaura205 <clinicalmedicine037@gmail.com>
2025-07-08 03:57:50 +0300

    Delete src directory

--- src/components/ui/button.jsx
+++
@@ -1,8 +0,0 @@
-export const Button = ({ children, className = '', ...props }) => (
-  <button
-    className={`bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-xl transition-all ${className}`}
-    {...props}
-  >
-    {children}
-  </button>
-);
--- src/components/ui/input.jsx
+++
@@ -1,6 +0,0 @@
-export const Input = ({ className = '', ...props }) => (
-  <input
-    className={`w-full px-3 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-400 ${className}`}
-    {...props}
-  />
-);
